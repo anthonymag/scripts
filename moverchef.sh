71c1a32023f505b06cce7212a28c51d8783a3d55
Anthony Magalhães <anthony.magal@gmail.com>
2024-06-14 15:53:56 -0400

    Add discord notifications

--- moverchef.sh
+++ moverchef.sh
@@ -2,16 +2,23 @@
 IFS=$'\n\t'
 
 SEASON_NUMBER="11"
+#SEASON_NUMBER="99" # for testing
 #PLAYLIST_PATH="/mnt/media/ytdlm/subscriptions/playlists/masterchef-br-${SEASON_NUMBER}/"
 PLAYLIST_PATH="/mnt/media/ytdlm/users/anthony/subscriptions/playlists/masterchef-br-${SEASON_NUMBER}/"
 FILE_PATH="/home/ant/mcbr-${SEASON_NUMBER}-files"
 MEDIA_PATH="/mnt/media/tvshows/MasterChef (BR) (2014) [tvdbid-285626]/Season ${SEASON_NUMBER}"
 PROCESSED_FILE_PATH="${PLAYLIST_PATH}processed"
 
+notify_discord() {
+  [ -n ${DISCORD_WEBHOOK} ] && curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"$1\"}" ${DISCORD_WEBHOOK}
+}
+
 if ls "$PLAYLIST_PATH"/*.mp4 1> /dev/null 2>&1; then
     echo "Found files to process"
+    notify_discord "Found MasterChef files to process"
 else
     echo "Nothing to do"
+    notify_discord "No MasterChef files found to process"
     exit 0
 fi
 
@@ -19,6 +26,7 @@ episodes="$(ls ${PLAYLIST_PATH}*.mp4 | awk '{print $2}' | uniq)"
 
 rm -rf "${FILE_PATH}"
 mkdir -p "${FILE_PATH}" "${PROCESSED_FILE_PATH}"
+mkdir -p "${MEDIA_PATH}"
 
 for ep in $episodes; do
   parts="$(ls ${PLAYLIST_PATH}EPISÓDIO\ ${ep}*.mp4)"
@@ -42,6 +50,7 @@ for episode_file in ${FILE_PATH}/episode*; do
   concat_episode_file_name="${MEDIA_PATH}/MasterChef (BR) (2014) - S${SEASON_NUMBER}E${episode_number} - TBA [HDTV-1080p][Opus 2.0][VP9].mp4"
 
   ffmpeg -f concat -safe 0 -i "${episode_file}" -c copy "${concat_episode_file_name}"
+  notify_discord "Processed ${base_file} as ${concat_episode_file_name}"
 
   if [ -f "${concat_episode_file_name}" ]; then
     echo "Moving ${base_file} parts to processed directory"
@@ -50,4 +59,6 @@ for episode_file in ${FILE_PATH}/episode*; do
 
   echo "*** DONE PROCESSING ${base_file} ***"
 done
+
+notify_discord "MoverChef job finished"
 
